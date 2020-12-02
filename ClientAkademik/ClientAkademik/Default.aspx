a82f22c016d62b2486179dd5463dea98e94c0204
fdlrswlly <72749894+fdlrswlly@users.noreply.github.com>
2020-12-02 09:38:10 +0700

    Add files via upload

---
+++ ClientAkademik/ClientAkademik/Default.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ClientAkademik._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <asp:Label ID="Label1" runat="server" Text="Sistem Akademik"></asp:Label>
+    <br />
+    <div>
+    
+        <asp:Label ID="Label2" runat="server" Text="Masukan NIM"></asp:Label>
+        <asp:TextBox ID="Txt_nim" runat="server"></asp:TextBox>
+        <asp:Button ID="btn_nim" runat="server" onclick="btn_nim_Click" 
+            Text="Tampilkan" />
+        <br />
+        <asp:Label ID="lbl_msg" runat="server" Text="Data "></asp:Label>
+        <br />
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="Masukkan NIM"></asp:Label>
+        &nbsp;<asp:TextBox ID="Txt_nn" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="Masukan Nama"></asp:Label>
+        &nbsp;<asp:TextBox ID="Txt_nama" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label5" runat="server" Text="Masukan Alamat"></asp:Label>
+        &nbsp;<asp:TextBox ID="Txt_alamat" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label6" runat="server" Text="Masukan Email"></asp:Label>
+        &nbsp;
+        <asp:TextBox ID="Txt_email" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label7" runat="server" Text="Masukan Nilai"></asp:Label>
+        &nbsp;<asp:TextBox ID="Txt_nilai" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="btn_simpan" runat="server" onclick="btn_simpan_Click" 
+            Text="SIMPAN" />
+        <asp:Button ID="btn_ubah" runat="server" onclick="btn_ubah_Click" Text="Ubah" />
+        <asp:Button ID="btn_hapus" runat="server" Text="Hapus" 
+            onclick="btn_hapus_Click" />
+        <br />
+        <br />
+    
+    </div>
+    <asp:GridView ID="Akademik_GD" runat="server">
+    </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ ClientAkademik/ClientAkademik/Default.aspx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ClientAkademik
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void btn_nim_Click(object sender, EventArgs e)
+        {
+            AkademikReference.Service1SoapClient obj = new AkademikReference.Service1SoapClient();
+            DataSet dsObj = obj.QueryModel(Txt_nim.Text.Trim());
+            if(dsObj.Tables[0].Rows.Count == 0)
+            {
+                lbl_msg.Text="Data tidak di temukan!";
+
+            }
+            else
+            {
+                Akademik_GD.DataSource =dsObj.Tables[0];
+                lbl_msg.Text = "Data di temukan";
+                Txt_nn.Text = dsObj.Tables[0].Rows[0][0].ToString();
+                Txt_nn.DataBind();
+                Txt_nama.Text = dsObj.Tables[0].Rows[0][1].ToString();
+                Txt_nama.DataBind();
+                Txt_alamat.Text = dsObj.Tables[0].Rows[0][2].ToString();
+                Txt_alamat.DataBind();
+                Txt_email.Text = dsObj.Tables[0].Rows[0][3].ToString();
+                Txt_email.DataBind();
+                Txt_nilai.Text = dsObj.Tables[0].Rows[0][4].ToString();
+                Txt_nilai.DataBind();
+            }
+            Akademik_GD.DataBind();
+
+    }
+
+        protected void btn_simpan_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                AkademikReference.Service1SoapClient obj = new AkademikReference.Service1SoapClient();
+                obj.insertdata(Txt_nn.Text, Txt_nama.Text, Txt_alamat.Text, Txt_email.Text, Convert.ToInt16(Txt_nilai.Text));
+                lbl_msg.Visible = true;
+                lbl_msg.Text = "Data Tersimpan";
+
+                DataSet dsObj = new DataSet();
+                dsObj = obj.Display();
+                
+                Akademik_GD.DataSource = dsObj.Tables[0];
+                Akademik_GD.DataBind();
+                
+
+            }
+            catch (Exception ex)
+            {
+                lbl_msg.Visible = true;
+                lbl_msg.Text = ex.Message.ToString();
+            }
+            
+        }
+
+        protected void btn_ubah_Click(object sender, EventArgs e)
+        {
+            AkademikReference.Service1SoapClient obj = new AkademikReference.Service1SoapClient();
+            obj.UbahAkademik(Txt_nn.Text, Txt_nama.Text, Txt_alamat.Text, Txt_email.Text, Convert.ToInt16(Txt_nilai.Text));
+            lbl_msg.Visible = true;
+            lbl_msg.Text = "Data Terupdate";
+
+            DataSet dsObj = new DataSet();
+            dsObj = obj.Display();
+
+            Akademik_GD.DataSource = dsObj.Tables[0];
+            Akademik_GD.DataBind();
+        }
+
+        protected void btn_hapus_Click(object sender, EventArgs e)
+        {
+            AkademikReference.Service1SoapClient obj = new AkademikReference.Service1SoapClient();
+            obj.DeleteAkademik(Txt_nn.Text);
+            lbl_msg.Visible = true;
+            lbl_msg.Text = "Data Terhapus";
+            DataSet dsObj = new DataSet();
+            dsObj = obj.Display();
+
+            Akademik_GD.DataSource = dsObj.Tables[0];
+            Akademik_GD.DataBind();
+        }
+    }
+}
